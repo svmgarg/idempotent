2e9f558435df3e03e860a1d9fa4b1dd32efdee7f
svmgarg <svmgarg@gmail.com>
2026-01-30 15:43:02 +0000

    Fixing Tastcase

--- idempotent-service/src/main/java/com/idempotent/dto/IdempotencyResponse.java
+++ idempotent-service/src/main/java/com/idempotent/dto/IdempotencyResponse.java
@@ -1,6 +1,7 @@
 package com.idempotent.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +17,11 @@ import java.time.Instant;
 public class IdempotencyResponse {
 
     private String idempotencyKey;
+    
+    @JsonProperty("isNew")
     private boolean isNew;
+    
+    @JsonProperty("isDuplicate")
     private boolean isDuplicate;
     private Instant createdAt;
     private Instant expiresAt;
